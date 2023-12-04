298ee3c92b2fb559c77d04ba4fcb37cd8b33ea64
BrunoSupriano <128910792+BrunoSupriano@users.noreply.github.com>
2023-12-04 15:02:58 -0300

    Alterado style da pagina de registro e corrigido tela de boas vindas

--- src/pages/Home/Estacionamentos.jsx
+++ src/pages/Home/Estacionamentos.jsx
@@ -11,6 +11,14 @@ export default function DetalheVaga() {
     const browserSpecificStyles = {
         WebkitBoxShadow: '0px 0px 7px 1px rgba(0, 111, 255, 1)',
         MozBoxShadow: '0px 0px 7px 1px rgba(0, 111, 255, 1)',
+        color: 'white'
+    };
+
+    const buttonneonstyles = {
+        WebkitBoxShadow: '0px 0px 5px 0px var(--azulclaroapp)',
+        backgroundColor: 'var(--pretopuro)',
+        color: 'var(--brancopuro)',
+        borderRadius: '8px',
     };
 
     const cardStyle = {
@@ -19,11 +27,12 @@ export default function DetalheVaga() {
         margin: 'auto',
         marginBottom: 2,
         ...browserSpecificStyles,
+        background: 'linear-gradient(to right, var(--azulclaroapp), var(--azulescuroapp))',
     };
 
     return (
         <div>
-            <h1 style={{maxWidth: 500, margin: 'auto', marginBottom: 10, borderRadius: '10px', textAlign: 'center'}}>Estacionamentos Disponiveis</h1>
+            <h1 style={{ maxWidth: 500, margin: 'auto', marginBottom: 10, borderRadius: '10px', textAlign: 'center' }}>Estacionamentos Disponiveis</h1>
             {items.map((vaga) => (
                 <Card key={vaga.id} sx={cardStyle}>
                     <CardContent>
@@ -31,11 +40,12 @@ export default function DetalheVaga() {
                         <h5>{vaga.localizacao}</h5>
                         <p>Horário de funcionamento: {vaga.horario}</p>
                         <p>Contato: {vaga.contato}</p>
-                        <p>Valores:</p>
-                        <p>1h - {vaga.valorhora}</p>
-                        <div style={{ marginTop: 'auto', textAlign: 'right' }}>
+                        <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <div style={{backgroundColor:'White', borderRadius:'10px', padding:'5px', alignItems:'center', textAlign:'center', fontWeight: 'bold'}}>
+                                <p style={{color:'var(--azulclaroapp)'}}>R${vaga.valorhora}/Hr</p>
+                            </div>
                             <Link to={`/vaga/${vaga.id}`}>
-                                <Button style={{ backgroundColor: 'var(--azulclaroapp)', borderRadius: '10px' }} variant="contained" >Reservar</Button>
+                                <Button style={buttonneonstyles} variant="contained">Reservar</Button>
                             </Link>
                         </div>
                     </CardContent>
